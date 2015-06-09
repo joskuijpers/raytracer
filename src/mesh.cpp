d437e3a0f1b754956a078d1b7a0906c8683d5211
Jos Kuijpers <jos@kuijpersvof.nl>
2015-06-10 01:02:21 +0200

    Typo thingie.

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -164,7 +164,7 @@ color3 mesh::apply(unsigned int level [[gnu::unused]], hit_result hit_info)
     // actual shading with all the gathered information.
 
     // Only grab diffuse color
-    vector3f diffuse = mat.getKd();g
+    vector3f diffuse = mat.getKd();
 
     return color3(diffuse);
 }
