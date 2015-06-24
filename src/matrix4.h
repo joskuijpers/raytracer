66e3ae0b7c3d016ffe0a63695df1503cf9aaf717
Marciano <M.R.F.Jorden@student.tudelft.nl>
2015-06-24 15:45:46 +0200

    Fix rotation issues

--- src/matrix4.h
+++ src/matrix4.h
@@ -397,24 +397,33 @@ public:
     }
 
     inline const Matrix4 makeRotation(float angle, T x, T y, T z) {
+        Vector3f normalized = Vector3f(x,y,z);
+        normalized.normalize();
+        
+        T nx = normalized[0];
+        T ny = normalized[1];
+        T nz = normalized[2];
+        
         T c = cosf(angle * DEG2RAD);
         T s = sinf(angle * DEG2RAD);
         T c1 = 1.f - c;
-
+        
         T m0 = m[0],  m4 = m[4],  m8 = m[8],  m12= m[12],
         m1 = m[1],  m5 = m[5],  m9 = m[9],  m13= m[13],
         m2 = m[2],  m6 = m[6],  m10= m[10], m14= m[14];
-
+        
         // build rotation matrix
-        T r0 = x * x * c1 + c;
-        T r1 = x * y * c1 + z * s;
-        T r2 = x * z * c1 - y * s;
-        T r4 = x * y * c1 - z * s;
-        T r5 = y * y * c1 + c;
-        T r6 = y * z * c1 + x * s;
-        T r8 = x * z * c1 + y * s;
-        T r9 = y * z * c1 - x * s;
-        T r10= z * z * c1 + c;
+        T r0 = nx * nx * c1 + c;
+        T r1 = nx * ny * c1 + nz * s;
+        T r2 = nx * nz * c1 - ny * s;
+        
+        T r4 = nx * ny * c1 - nz * s;
+        T r5 = ny * ny * c1 + c;
+        T r6 = ny * nz * c1 + nx * s;
+        
+        T r8 = nx * nz * c1 + ny * s;
+        T r9 = ny * nz * c1 - nx * s;
+        T r10= nz * nz * c1 + c;
 
         // multiply rotation matrix
         m[0] = r0 * m0 + r4 * m1 + r8 * m2;
